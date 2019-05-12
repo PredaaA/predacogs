540126be85ba679d0abf6791f5da676c1dfc9986
predaAa <46051820+predaAa@users.noreply.github.com>
2019-05-12 21:31:17 +0200

    [Converters] Add i18n support

--- converters/converters.py
+++ converters/converters.py
@@ -1,18 +1,22 @@
 import discord
 
 from redbot.core import commands
+from redbot.core.i18n import Translator, cog_i18n
 from redbot.core.utils.chat_formatting import humanize_timedelta
 
 from datetime import datetime
 
 import contextlib
 
+_ = Translator("DblTools", __file__)
 
+
+@cog_i18n(_)
 class Converters(commands.Cog):
     """Some converters."""
 
     __author__ = "Predä"
-    __version__ = "0.1"
+    __version__ = "0.2"
 
     def __init__(self, bot):
         self.bot = bot
@@ -29,15 +33,21 @@ class Converters(commands.Cog):
             given = timestamp[: timestamp.find(".")] if "." in timestamp else timestamp
             convert = datetime.utcfromtimestamp(int(given)).strftime("%Y-%m-%d %H:%M:%S")
         except (ValueError, OverflowError):
-            return await ctx.send(f"`{given}` is not a valid timestamp.")
-        b = datetime.fromtimestamp(int(given))
+            return await ctx.send(_("`{}` is not a valid timestamp.").format(given))
+        g = datetime.fromtimestamp(int(given))
         curr = datetime.fromtimestamp(int(datetime.now().timestamp()))
-        secs = str((curr - b).total_seconds())
+        secs = str((curr - g).total_seconds())
         seconds = secs[1:][:-2] if "-" in secs else secs[:-2] if ".0" in secs else secs
         delta = humanize_timedelta(seconds=int(seconds))
-        to = f"It will be in {delta}." if b > curr else f"It was {delta} ago."
+        to = (
+            _("It will be in {}.").format(delta) if g > curr else _("It was {} ago.").format(delta)
+        )
 
-        await ctx.send(f"Successfully converted `{given}` to `{convert}`\n{to}")
+        await ctx.send(
+            _("Successfully converted `{given}` to `{convert}`\n{to}").format(
+                given=given, convert=convert, to=to
+            )
+        )
 
     @convert.command()
     async def tounix(self, ctx, *, date):
@@ -66,14 +76,22 @@ class Converters(commands.Cog):
         try:
             given = datetime.fromtimestamp(int(convert))
         except UnboundLocalError:
-            return await ctx.send(f"`{date}` is not a valid timestamp.")
+            return await ctx.send(_("`{}` is not a valid timestamp.").format(date))
         curr = datetime.fromtimestamp(int(datetime.now().timestamp()))
         secs = str((curr - given).total_seconds())
         seconds = secs[1:][:-2] if "-" in secs else secs[:-2] if ".0" in secs else secs
         delta = humanize_timedelta(seconds=int(seconds))
-        to = f"It will be in {delta}." if given > curr else f"It was {delta} ago."
+        to = (
+            _("It will be in {}.").format(delta)
+            if given > curr
+            else _("It was {} ago.").format(delta)
+        )
 
-        await ctx.send(f"Successfully converted `{date}` to `{convert}`\n{to}")
+        await ctx.send(
+            _("Successfully converted `{date}` to `{convert}`\n{to}").format(
+                date=date, convert=convert, to=to
+            )
+        )
 
     @convert.command(aliases=["celsiustofahrenheit", "ctof", "celsiustokelvin", "ctok"])
     async def celsius(self, ctx, temperature: float):
@@ -91,10 +109,14 @@ class Converters(commands.Cog):
             return await ctx.send_help()
         elif ctx.invoked_with in ["celsiustofahrenheit", "ctof"]:
             fahrenheit = round((temperature * 1.8) + 32, 1)
-            msg = f"{temperature:,}° Celsius is equal to {fahrenheit:,}° Fahrenheit."
+            msg = _("{temp:,}° Celsius is equal to {f:,}° Fahrenheit.").format(
+                temp=temperature, f=fahrenheit
+            )
         elif ctx.invoked_with in ["celsiustokelvin", "ctok"]:
             kelvin = round((temperature + 273.15), 1)
-            msg = f"{temperature:,}° Celsius is equal to {kelvin:,}° Kelvin."
+            msg = _("{temp:,}° Celsius is equal to {k:,}° Kelvin.").format(
+                temp=temperature, k=kelvin
+            )
         await ctx.send(msg)
 
     @convert.command(aliases=["fahrenheittocelsius", "ftoc", "fahrenheittokelvin", "ftok"])
@@ -113,10 +135,14 @@ class Converters(commands.Cog):
             return await ctx.send_help()
         elif ctx.invoked_with in ["fahrenheittocelsius", "ftoc"]:
             celsius = round((temperature - 32) / 1.8, 1)
-            msg = f"{temperature:,}° Fahrenheit is equal to {celsius:,}° Celsius."
+            msg = _("{temp:,}° Fahrenheit is equal to {c:,}° Celsius.").format(
+                temp=temperature, c=celsius
+            )
         elif ctx.invoked_with in ["fahrenheittokelvin", "ftok"]:
             kelvin = round((temperature - 32) * (5 / 9) + 273.15, 1)
-            msg = f"{temperature:,}° Fahrenheit is equal to {kelvin:,}° Kelvin."
+            msg = _("{temp:,}° Fahrenheit is equal to {k:,}° Kelvin.").format(
+                temp=temperature, k=kelvin
+            )
         await ctx.send(msg)
 
     @convert.command(aliases=["kelvintofahrenheit", "ktof", "kelvintocelsius", "ktoc"])
@@ -134,33 +160,37 @@ class Converters(commands.Cog):
         elif not ctx.invoked_with in ["kelvintofahrenheit", "ktof", "kelvintocelsius", "ktoc"]:
             return await ctx.send_help()
         elif ctx.invoked_with in ["kelvintofahrenheit", "ktof"]:
-            kelvin = round((temperature - 273.15) * (9 / 5) + 32, 1)
-            msg = f"{temperature:,}° Kelvin is equal to {kelvin:,}° Fahrenheit."
+            fahrenheit = round((temperature - 273.15) * (9 / 5) + 32, 1)
+            msg = _("{temp:,}° Kelvin is equal to {f:,}° Fahrenheit.").format(
+                temp=temperature, f=fahrenheit
+            )
         elif ctx.invoked_with in ["kelvintocelsius", "ktoc"]:
-            kelvin = round((temperature - 273.15), 1)
-            msg = f"{temperature:,}° Kelvin is equal to {kelvin:,}° Celsius."
+            celsius = round((temperature - 273.15), 1)
+            msg = _("{temp:,}° Kelvin is equal to {c:,}° Celsius.").format(
+                temp=temperature, c=celsius
+            )
         await ctx.send(msg)
 
     @convert.command(aliases=["lb"])
     async def pounds(self, ctx, mass: float):
         """Convert pounds to kilograms."""
         kg = round((mass * 0.45359237), 1)
-        await ctx.send(f"{mass:,} lb is equal to {kg:,} kg.")
+        await ctx.send(_("{mass:,} lb is equal to {kg:,} kg.").format(mass=mass, kg=kg))
 
     @convert.command(aliases=["kg"])
     async def kilograms(self, ctx, mass: float):
         """Convert kilograms to pounds."""
         lb = round((mass / 0.45359237), 1)
-        await ctx.send(f"{mass:,} kg is equal to {lb:,} lb.")
+        await ctx.send(_("{mass:,} kg is equal to {lb:,} lb.").format(mass=mass, lb=lb))
 
     @convert.command(aliases=["mi"])
     async def miles(self, ctx, lenght: float):
         """Convert miles to kilometers"""
         km = round((lenght * 1.609344), 1)
-        await ctx.send(f"{lenght:,} mi is equal to {km:,} km.")
+        await ctx.send(_("{lenght:,} mi is equal to {km:,} km.").format(lenght=lenght, km=km))
 
     @convert.command(aliases=["km"])
     async def kilometers(self, ctx, lenght: float):
         """Convert kilometers to miles."""
         mi = round((lenght / 1.609344), 1)
-        await ctx.send(f"{lenght:,} km is equal to {mi:,} mi.")
+        await ctx.send(_("{lenght:,} km is equal to {mi:,} mi.").format(lenght=lenght, mi=mi))

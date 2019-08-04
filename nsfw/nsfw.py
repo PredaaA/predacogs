a3547759bb966a683753ad86dd27bb47dd550bba
predaAa <46051820+predaAa@users.noreply.github.com>
2019-08-04 19:32:01 +0200

    [Nsfw] 2.2

--- nsfw/nsfw.py
+++ nsfw/nsfw.py
@@ -21,7 +21,7 @@ class Nsfw(Core, commands.Cog):
     """
 
     __author__ = ["Predä", "aikaterna"]
-    __version__ = "2.1.3"
+    __version__ = "2.2"
 
     @commands.command()
     async def nsfwversion(self, ctx):

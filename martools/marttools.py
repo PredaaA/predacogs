517dad5cf5558f7081df6bec5a7ae46257a00224
guyre <27962761+drapersniper@users.noreply.github.com>
2019-09-06 22:39:54 +0100

    Update marttools in prop for new Red release

--- martools/marttools.py
+++ martools/marttools.py
@@ -1,88 +1,249 @@
-import discord
+import asyncio
+import contextlib
+from collections import Counter
+from copy import copy
+from datetime import datetime
 
+import discord
 import lavalink
 
-from redbot.core import bank, commands, checks
+from redbot.core import Config, bank, commands
 from redbot.core.i18n import Translator, cog_i18n
 from redbot.core.utils.chat_formatting import bold, humanize_timedelta
-# from redbot.cogs.audio.localtracks import Query <- Will be probably changed
 
-from datetime import datetime
+# from redbot.cogs.audio.dataclasses import Query
 
 _ = Translator("MartTools", __file__)
 
 
 @cog_i18n(_)
 class MartTools(commands.Cog):
-    """Multiple tools that are originally used on Martine."""
+    """Multiple tools that are originally used on Martine the BOT."""
 
     __author__ = "Predä"
-    __version__ = "1.4.1"
+    __version__ = "1.4.0"
 
     def __init__(self, bot):
         self.bot = bot
+        self.counter = Counter()
+        self.sticky_counter = Counter()
+        self.config = Config.get_conf(self, 64481875498, force_registration=True)
+        global_defauls = dict(
+            command_error=0,
+            msg_sent=0,
+            dms_received=0,
+            messages_read=0,
+            guild_join=0,
+            guild_remove=0,
+            sessions_resumed=0,
+            processed_commands=0,
+            new_members=0,
+            members_left=0,
+            messages_deleted=0,
+            messages_edited=0,
+            reactions_added=0,
+            reactions_removed=0,
+            roles_added=0,
+            roles_removed=0,
+            roles_updated=0,
+            members_banned=0,
+            members_unbanned=0,
+            emojis_removed=0,
+            emojis_added=0,
+            emojis_updated=0,
+            users_joined_bot_music_room=0,
+        )
+        self.config.register_global(**global_defauls)
+        self._task = self.bot.loop.create_task(self._save_counters_to_config())
         lavalink.register_event_listener(self.event_handler)  # To delete at next audio update.
 
-    def cog_unload(self):  # To delete at next audio update.
-        lavalink.unregister_event_listener(self.event_handler)
+    def cog_unload(self):
+        lavalink.unregister_event_listener(self.event_handler)  # To delete at next audio update.
+        self.bot.loop.create_task(self._clean_up())
 
     async def event_handler(self, player, event_type, extra):  # To delete at next audio update.
         # Thanks Draper#6666
         if event_type == lavalink.LavalinkEvents.TRACK_START:
-            self.bot.counter["tracks_played"] += 1
+            self.update_counters("tracks_played")
+
+    def update_counters(self, key: str):
+        self.counter[key] += 1
+        self.sticky_counter[key] += 1
 
     def get_bot_uptime(self):
-        delta = datetime.utcnow() - self.bot.uptime
+        delta = datetime.utcnow() - (
+            self.bot.uptime if hasattr(self.bot, "uptime") else self.bot._uptime
+        )
         uptime = humanize_timedelta(timedelta=delta)
         return uptime
 
     @commands.Cog.listener()
-    async def on_command_error(self, ctx, error):
-        if isinstance(error, commands.CommandInvokeError):
-            self.bot.counter["command_error"] += 1
+    async def on_command_error(self, ctx, error, unhandled_by_cog=False):
+        if not unhandled_by_cog:
+            if hasattr(ctx.command, "on_error"):
+                return
+
+            if ctx.cog:
+                if commands.Cog._get_overridden_method(ctx.cog.cog_command_error) is not None:
+                    return
+        if isinstance(
+            error,
+            (
+                commands.MissingRequiredArgument,
+                commands.ConversionFailure,
+                commands.UserInputError,
+                commands.DisabledCommand,
+                commands.CommandNotFound,
+                commands.BotMissingPermissions,
+                commands.UserFeedbackCheckFailure,
+                commands.CheckFailure,
+                commands.NoPrivateMessage,
+                commands.CommandOnCooldown,
+                getattr(commands, "ArgParserFailure", commands.UserInputError),
+            ),
+        ):
+            return
+        self.update_counters("command_error")
 
     @commands.Cog.listener()
-    async def on_message(self, message):
+    async def on_message(self, message: discord.Message):
         if message.author.id == self.bot.user.id:
-            self.bot.counter["msg_sent"] += 1
+            self.update_counters("msg_sent")
+        if message.guild is None:
+            self.update_counters("dms_received")
+        self.update_counters("messages_read")
 
     @commands.Cog.listener()
     async def on_guild_join(self, guild: discord.Guild):
-        self.bot.counter["guild_join"] += 1
+        self.update_counters("guild_join")
 
     @commands.Cog.listener()
     async def on_guild_remove(self, guild: discord.Guild):
-        self.bot.counter["guild_remove"] += 1
+        self.update_counters("guild_remove")
 
-    # Planned for next audio update.
+    @commands.Cog.listener()
+    async def on_resumed(self):
+        self.update_counters("sessions_resumed")
+
+    @commands.Cog.listener()
+    async def on_command(self, command):
+        self.update_counters("processed_commands")
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        self.update_counters("new_members")
+
+    @commands.Cog.listener()
+    async def on_member_remove(self, member):
+        self.update_counters("members_left")
+
+    @commands.Cog.listener()
+    async def on_message_delete(self, message):
+        self.update_counters("messages_deleted")
+
+    @commands.Cog.listener()
+    async def on_message_edit(self, before, after):
+        self.update_counters("messages_edited")
+
+    @commands.Cog.listener()
+    async def on_reaction_add(self, reaction, user):
+        self.update_counters("reactions_added")
+
+    @commands.Cog.listener()
+    async def on_reaction_remove(self, reaction, user):
+        self.update_counters("reactions_removed")
+
+    @commands.Cog.listener()
+    async def on_guild_role_create(self, role):
+        self.update_counters("roles_added")
+
+    @commands.Cog.listener()
+    async def on_guild_role_delete(self, role):
+        self.update_counters("roles_removed")
+
+    @commands.Cog.listener()
+    async def on_guild_role_update(self, role):
+        self.update_counters("roles_updated")
+
+    @commands.Cog.listener()
+    async def on_member_ban(self, guild, user):
+        self.update_counters("members_banned")
+
+    @commands.Cog.listener()
+    async def on_member_unban(self, guild, user):
+        self.update_counters("members_unbanned")
+
+    @commands.Cog.listener()
+    async def on_guild_emojis_update(self, guild, before, after):
+        if len(before) > len(after):
+            self.update_counters("emojis_removed")
+        elif len(before) < len(after):
+            self.update_counters("emojis_added")
+        else:
+            self.update_counters("emojis_updated")
+
+    @commands.Cog.listener()
+    async def on_voice_state_update(
+        self, member, before: discord.VoiceState, after: discord.VoiceState
+    ):
+        if not after.channel:
+            return
+        guild = after.channel.guild
+        bot_in_room = guild.me in after.channel.members
+        if bot_in_room:
+            self.update_counters("users_joined_bot_music_room")
+
     # @commands.Cog.listener()
-    # async def on_track_start(self, guild, track, requester):
-    #     self.bot.counter["tracks_played"] += 1
-    #     query = Query.process_input(track.uri)
+    # async def on_red_audio_track_start(self, guild, track, requester):
+    #     self.update_counters("tracks_played")
+    #     query = Query.process_input(track)
+    #
     #     if track.is_stream:
-    #         self.bot.counter["streams_played"] += 1
-    #     if track.is_stream and query.is_youtube:
-    #         self.bot.counter["yt_streams_played"] += 1
-    #     if track.is_stream and query.is_mixer:
-    #         self.bot.counter["mixer_streams_played"] += 1
-    #     if track.is_stream and query.is_twitch:
-    #         self.bot.counter["ttv_streams_played"] += 1
-    #     if track.is_stream and query.is_other:
-    #         self.bot.counter["other_streams_played"] += 1
+    #         self.update_counters("streams_played")
+    #         if query.is_youtube:
+    #             self.update_counters("yt_streams_played")
+    #         elif query.is_mixer:
+    #             self.update_counters("mixer_streams_played")
+    #         elif query.is_twitch:
+    #             self.update_counters("ttv_streams_played")
+    #         elif query.is_other:
+    #             self.update_counters("other_streams_played")
+    #
     #     if query.is_youtube:
-    #         self.bot.counter["youtube_tracks"] += 1
-    #     if query.is_soundcloud:
-    #         self.bot.counter["soundcloud_tracks"] += 1
-    #     if query.is_bandcamp:
-    #         self.bot.counter["bandcamp_tracks"] += 1
-    #     if query.is_vimeo:
-    #         self.bot.counter["vimeo_tracks"] += 1
-    #     if query.is_mixer:
-    #         self.bot.counter["mixer_tracks"] += 1
-    #     if query.is_twitch:
-    #         self.bot.counter["twitch_tracks"] += 1
-    #     if query.is_other:
-    #         self.bot.counter["other_tracks"] += 1
+    #         self.update_counters("youtube_tracks")
+    #     elif query.is_soundcloud:
+    #         self.update_counters("soundcloud_tracks")
+    #     elif query.is_bandcamp:
+    #         self.update_counters("bandcamp_tracks")
+    #     elif query.is_vimeo:
+    #         self.update_counters("vimeo_tracks")
+    #     elif query.is_mixer:
+    #         self.update_counters("mixer_tracks")
+    #     elif query.is_twitch:
+    #         self.update_counters("twitch_tracks")
+    #     elif query.is_other:
+    #         self.update_counters("other_tracks")
+
+    async def _save_counters_to_config(self):
+        await self.bot.wait_until_ready()
+        with contextlib.suppress(asyncio.CancelledError):
+            while True:
+                _date = datetime.utcnow().timestamp()
+                users_data = copy(self.sticky_counter)
+                self.sticky_counter = Counter()
+                async with self.config.all() as new_data:
+                    for key, value in users_data.items():
+                        new_data[key] += value
+                    if "start_date" not in new_data:
+                        new_data["start_date"] = _date
+                await asyncio.sleep(60)
+
+    async def _clean_up(self):
+        if self._task:
+            self._task.cancel()
+        async with self.config.all() as new_data:
+            for key, value in self.sticky_counter.items():
+                new_data[key] += value
 
     @commands.command()
     @commands.guild_only()
@@ -144,10 +305,10 @@ class MartTools(commands.Cog):
             Commands processed, messages received, and music on servers.
         """
         uptime = str(self.get_bot_uptime())
-        commands_count = "`{:,}`".format(self.bot.counter["processed_commands"])
-        errors_count = "`{:,}`".format(self.bot.counter["command_error"])
-        messages_read = "`{:,}`".format(self.bot.counter["messages_read"])
-        messages_sent = "`{:,}`".format(self.bot.counter["msg_sent"])
+        commands_count = "`{:,}`".format(self.counter["processed_commands"])
+        errors_count = "`{:,}`".format(self.counter["command_error"])
+        messages_read = "`{:,}`".format(self.counter["messages_read"])
+        messages_sent = "`{:,}`".format(self.counter["msg_sent"])
         try:
             total_num = "`{:,}/{:,}`".format(
                 len(lavalink.active_players()), len(lavalink.all_players())
@@ -157,9 +318,9 @@ class MartTools(commands.Cog):
                 len([p for p in lavalink.players if p.current is not None]),
                 len([p for p in lavalink.players]),
             )
-        tracks_played = "`{:,}`".format(self.bot.counter["tracks_played"])
-        guild_join = "`{:,}`".format(self.bot.counter["guild_join"])
-        guild_leave = "`{:,}`".format(self.bot.counter["guild_remove"])
+        tracks_played = "`{:,}`".format(self.counter["tracks_played"])
+        guild_join = "`{:,}`".format(self.counter["guild_join"])
+        guild_leave = "`{:,}`".format(self.counter["guild_remove"])
         avatar = self.bot.user.avatar_url_as(static_format="png")
         msg = (
             bold(_("Commands processed: "))

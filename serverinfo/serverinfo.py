0aebeb98da1702670712a42899627c75f3f2cb7d
Drapersniper <27962761+drapersniper@users.noreply.github.com>
2019-12-16 19:42:02 +0000

    Revert "Only add new field if in 3.2"
    This reverts commit b10d61d3

--- serverinfo/serverinfo.py
+++ serverinfo/serverinfo.py
@@ -153,7 +153,9 @@ class ServerInfo(commands.Cog):
             "MEMBER_LIST_DISABLED": _("Member list disabled"),
         }
         guild_features_list = [
-            f"\✅ {name}" for feature, name in features.items() if feature in set(guild.features)
+            f"\✅ {name}"
+            for feature, name in features.items()
+            if feature in set(guild.features)
         ]
 
         since_joined = (ctx.message.created_at - guild.me.joined_at).days

55dfc62ce8ae959b1f3db1f2963c0f72139f42cd
PredaMart <predaamart@outlook.com>
2020-03-09 13:06:59 +0100

    [Nsfw] Remove another subreddit.

--- nsfw/constants.py
+++ nsfw/constants.py
@@ -146,7 +146,7 @@ DEEPTHROAT = [
 ]
 DICK = ["DickPics4Freedom", "MassiveCock", "penis", "cock", "ThickDick", "bulges", "twinks"]
 DOUBLE_P = ["doublepenetration", "dp_porn", "Technical_DP"]
-FACIALS = ["facial", "EthnicGirlFacials", "facialcumshots", "FacialFun"]
+FACIALS = ["facial", "facialcumshots", "FacialFun"]
 FEET = ["ButtsAndBareFeet", "Feet_NSFW", "feetish", "Feetup", "rule34feet", "StomachDownFeetUp"]
 FEMDOM = ["Femdom", "femdom", "FemdomHumiliation", "hentaifemdom"]
 FUTA = [

9f4133a93f4909151455b78eb6f0a8148e053f87
PhenoM4n4n <phenom4n4n@gmail.com>
2020-10-05 21:53:05 -0700

    command conflict

--- martools/__init__.py
+++ martools/__init__.py
@@ -1,3 +1,4 @@
+from asyncio import create_task
 from redbot.core.bot import Red
 from .marttools import MartTools
 
@@ -6,6 +7,19 @@ __red_end_user_data_statement__ = (
 )
 
 
-def setup(bot: Red):
+# from https://github.com/fixator10/Fixator10-Cogs/blob/V3/adminutils/__init__.py
+async def setup_after_ready(bot: Red):
+    await bot.wait_until_red_ready()
     cog = MartTools(bot)
+    for name, command in cog.all_commands.items():
+        if not command.parent:
+            if bot.get_command(name):
+                command.name = f"m{command.name}"
+            for alias in command.aliases:
+                if bot.get_command(alias):
+                    command.aliases[command.aliases.index(alias)] = f"i{alias}"
     bot.add_cog(cog)
+
+
+def setup(bot: Red):
+    create_task(setup_after_ready(bot))
